700d041615df0419bd26bf6a789edab33ae010ca
connector26 <amey_choudhari@yahoo.com>
2025-12-17 11:53:57 +0530

    doc:ready for project

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ Pytest configuration for Django tests
 import os
 import django
 from django.conf import settings
-import pytest
 
 # Setup Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectmanagerdashboard.settings')
@@ -13,9 +12,3 @@ def pytest_configure(config):
     """Configure pytest for Django"""
     if not settings.configured:
         django.setup()
-
-@pytest.fixture(scope='session')
-def django_db_setup():
-    """Ensure database migrations are applied before tests"""
-    from django.core.management import call_command
-    call_command('migrate', '--noinput')
